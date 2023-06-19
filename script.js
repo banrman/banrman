5a03ac059cfb58ce01dd2e789cfe841e9056c315
BrandonKW <bran.weber546@gmail.com>
2023-06-18 21:44:28 -0500

    add quotes bubble

--- script.js
+++ script.js
@@ -13,10 +13,49 @@ function checkPassword() {
         setTimeout(function() {
             // Remove the loading screen once the game is loaded
             document.getElementById('loading-screen').style.display = 'none';
-        }, 3000);
+        }, 2000);
 
 
         window.addEventListener('DOMContentLoaded', () => {
             const audio = document.getElementById('bg-music');
-            audio.play();
+            const volumeSlider = document.getElementById('volume-slider');
+          
+            audio.volume = volumeSlider.value; // Set initial volume
+          
+            volumeSlider.addEventListener('input', () => {
+              audio.volume = volumeSlider.value; // Update volume when slider changes
+            });
+            
+              audio.play();
           });
+
+          document.addEventListener('DOMContentLoaded', function() {
+          const quotes = [
+            "More to come soon...",
+            "I feel a song coming on...",
+            "Am I forgetting something...",
+            "Time to reread One Piece..."
+          ];
+          
+          let currentQuoteIndex = 0;
+
+          function rotateQuotes() {
+            const quoteText = document.getElementById("quote-text");
+            const speechBubble = document.querySelector(".speech-bubble");
+          
+            // Set the quote text
+            quoteText.textContent = quotes[currentQuoteIndex];
+          
+            // Show the speech bubble
+            speechBubble.style.display = "block";
+          
+            // Increment the quote index
+            currentQuoteIndex = (currentQuoteIndex + 1) % quotes.length;
+          }
+          
+          // Call rotateQuotes function when the animation iteration starts
+          document.querySelector(".speech-bubble").addEventListener("animationiteration", rotateQuotes);
+          
+          // Initial quote rotation
+          rotateQuotes();
+        });
